ae78d8d2045809483f69653dbdf8c12e39ad84b5
minseunghwang <mins1011@hanmail.net>
2020-03-28 14:55:58 +0900

    20200328 algo1

---
+++ programmers/동적계획법 - 타일장식물.py
@@ -0,0 +1,12 @@
+def solution(N):
+    ms = [1, 1]
+    if N >= 3:
+        for i in range(N - 2):
+            ms.append(ms[-1] + ms[-2])
+
+    return ms[-1] * 2 + (ms[-2] + ms[-1]) * 2
+
+
+N = 5 # 26
+N = 6 # 42
+print(solution(N))
