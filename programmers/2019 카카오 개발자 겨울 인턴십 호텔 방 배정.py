7d13125fb3aa766dea97832b3c83603cc82f6ce5
minseunghwang <mins1011@hanmail.net>
2020-05-10 20:20:02 +0900

    20200510 algo

---
+++ programmers/2019 카카오 개발자 겨울 인턴십 호텔 방 배정.py
@@ -0,0 +1,24 @@
+
+def find_empty_room(x,rooms):
+    print('ㅋㅋ',x,rooms)
+    if x not in rooms:
+        rooms[x] = x + 1
+        return x
+    p = find_empty_room(rooms[x],rooms)
+    rooms[x] = p + 1
+    return p
+
+
+def solution(k, room_number):
+    answer = []
+    rooms = {}
+    for each_room in room_number:
+        empty_room = find_empty_room(each_room, rooms)
+        answer.append(empty_room)
+        print(answer)
+
+    return answer
+
+k = 100
+room_number = [1,3,4,1,3,1]
+print(solution(k, room_number))
